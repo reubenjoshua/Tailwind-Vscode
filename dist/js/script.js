f3600e2f851594c3e617bd33e4d4d88bd4d84e60
aaairam <magildaceria@gmail.com>
2023-08-02 15:31:40 +0800

    notyetdone

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.burgir');
+const drop = document.querySelector('.options__nav');
+
+toggleBtn.addEventListener('click', () =>{
+    drop.classList.toggle('h-[20rem]');
+});
